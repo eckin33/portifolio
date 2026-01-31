b86e81b37e99a4e5c9a6159b5b7847231431e8a6
eckin33 <erickdubisoj4@gmail.com>
2026-01-31 19:48:56 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -481,7 +481,7 @@ function App() {
 
                   <ul type='none' className='lista-ctt gap-6 mt-6 text-left pl-8 w-full h-32'>
 
-                    <a href="mailto:erickdubisoj4@gmail.com" target='_blank'><li className='li-ctt hover:scale-105 transition-transform'>
+                    <a href="mailto:erickdubisoj4@gmail.com?subject=Sauda%C3%A7%C3%B5es%2C%20Erick&body=Vim%20pelo%20seu%20portf%C3%B3lio%20e%20achei%20que..." target='_blank'><li className='li-ctt hover:scale-105 transition-transform'>
                       <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
                         <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z" />
                       </svg></li>
