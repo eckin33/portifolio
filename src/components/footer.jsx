c1ec9f4e82983a026d8a270590f92c801975f5ee
eckin33 <erickdubisoj4@gmail.com>
2026-03-02 15:35:36 -0300

    Add info sobre privacidade

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,8 +4,10 @@ import { useTranslation } from 'react-i18next'
 const Footer = () => {
     const { t } = useTranslation()
     return (
-        <footer className='w-full h-34 flex justify-center items-center border-t'>
+        <footer className='w-full h-34 flex flex-col justify-center items-center border-t'>
             <p>{t('direitos')}</p>
+            
+            <p className='text-sm mt-2' id='footer-info' >{t('footer-info')}</p>
         </footer>
     )
 }
